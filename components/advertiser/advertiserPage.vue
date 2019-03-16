894dc4260c1b520f79dd7afb934bc47065c9c04f
IlyaKukarkin <ilya1196@yandex.ru>
2019-03-16 20:03:37 +0500

    To the infinity and beyond

---
+++ components/advertiser/advertiserPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-row>
+    <el-col :span="8"><p/></el-col>
+    <el-col :span="8">
+      <br>
+      <br>
+      <br>
+      <br>
+      <div class="block">
+        <span class="demonstration">Оцените страницу</span>
+        <el-rate v-model="value1"/>
+      </div>
+    </el-col>
+    <el-col :span="8"><p/></el-col>
+  </el-row>
+</template>
+<script>
+export default {
+  name: 'AdvertiserPage',
+  data() {
+    return {
+      value1: null
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ pages/advertiser/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <advertiser-page/>
+  </div>
+</template>
+
+<script>
+import advertiserPage from '../../components/advertiser/advertiserPage'
+export default {
+  name: 'Advertiser',
+  components: { advertiserPage }
+}
+</script>
+
+<style scoped>
+</style>
